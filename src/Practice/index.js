531646d5dc6b99717f477106331f0e55e472b193
lab3 <lab3@tcslab.com>
2016-08-10 10:32:18 +0530

    React Practice - Day-2

--- src/Practice/index.js
+++ src/Practice/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './Practice/SearchBar';
+import SearchBar from './SearchBar';
 
 class App extends Component{
   constructor(props){
